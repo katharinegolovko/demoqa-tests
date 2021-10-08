b24f90c32ab9a78a3733beea5ec3603ad7c6378f
ekaterinagolovko <katharinegolovko@gmail.com>
2021-10-08 14:43:12 +0300

    Added changes for Jenkins-1 task

--- src/test/java/by/katyagolovko/tests/TestBase.java
+++ src/test/java/by/katyagolovko/tests/TestBase.java
@@ -1,13 +1,37 @@
 package by.katyagolovko.tests;
 
+import by.katyagolovko.helpers.Attach;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
         Configuration.baseUrl = "https://demoqa.com";
     }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+
+
 }
