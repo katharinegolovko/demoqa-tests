48487bdc657c8f864882f1987633702b233aa07e
ekaterinagolovko <katharinegolovko@gmail.com>
2021-10-12 22:51:17 +0300

    Hid Selenoid username and password in properties

--- src/test/java/by/katyagolovko/tests/TestBase.java
+++ src/test/java/by/katyagolovko/tests/TestBase.java
@@ -1,14 +1,20 @@
 package by.katyagolovko.tests;
 
+import by.katyagolovko.config.CredentialsConfig;
 import by.katyagolovko.helpers.Attach;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static java.lang.String.format;
+
 public class TestBase {
+
+    public static CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
 
     @BeforeAll
     static void beforeAll() {
@@ -19,9 +25,12 @@ public class TestBase {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
 
+        String username = credentials.username();
+        String password = credentials.password();
         Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        String selenoidURL = format("https://%s:$s@selenoid.autotests.cloud/wd/hub/", username, password);
+        Configuration.remote = selenoidURL;
         Configuration.baseUrl = "https://demoqa.com";
     }
 
